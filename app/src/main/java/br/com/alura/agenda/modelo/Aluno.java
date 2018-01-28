9ccaff5fa81052c1c62f7bed0f90ce0b03456317
diegodamato <diegodamato6@gmail.com>
2018-01-28 16:50:46 -0200

    Finalizando aula4

--- app/src/main/java/br/com/alura/agenda/modelo/Aluno.java
+++ app/src/main/java/br/com/alura/agenda/modelo/Aluno.java
@@ -59,4 +59,10 @@ public class Aluno {
     public void setNota(Double nota) {
         this.nota = nota;
     }
+
+    @Override
+    public String toString() {
+        return getId() + " - " + getNome();
+    }
 }
+
